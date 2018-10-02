8367ef729ff3bbd3262c14ad38064909c30aad87
Jacques Becker <beckej3@rpi.edu>
2018-10-02 04:28:22 -0400

    created room.model and room.controller

---
+++ server/api/room/room.controller.js
@@ -0,0 +1,19 @@
+'use strict';
+
+var _ = require('lodash');
+var Room = require('./room.model');
+
+
+//get room list
+
+//room search
+
+//create room
+
+//update room
+
+//delete room
+
+function handleError(res, err) {
+    return res.send(500, err);
+}
